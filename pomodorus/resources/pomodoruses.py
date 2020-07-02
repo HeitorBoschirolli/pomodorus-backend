15afa87099e48bdb7c9f26a93673aae469c1e162
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2020-07-02 20:09:11 -0300

    Filter pomodoros list by user

--- pomodorus/resources/pomodoruses.py
+++ pomodorus/resources/pomodoruses.py
@@ -4,6 +4,7 @@ sessions.
 """
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
 
 from pomodorus.models.pomodoro import Pomodoro as PomodoroModel
 
@@ -13,6 +14,9 @@ class Pomodoruses(Resource):
     @staticmethod
     @jwt_required
     def get():
+        user_id = get_jwt_identity()
         return {
-            'pomodoros': [p.json() for p in PomodoroModel.find_all()]
+            'pomodoros': [
+                p.json() for p in PomodoroModel.find_by_user_id(user_id)
+            ]
         }
