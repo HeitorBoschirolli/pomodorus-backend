25c834dd68efd4effeca69407a8d50abd7915da7
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2020-07-02 12:17:11 -0300

    Require jwt to get a list of intervals

--- pomodorus/resources/pomodoruses.py
+++ pomodorus/resources/pomodoruses.py
@@ -3,6 +3,7 @@ Pomodoruses resource. It contains information about many pomodoro or rest
 sessions.
 """
 from flask_restful import Resource
+from flask_jwt_extended import jwt_required
 
 from pomodorus.models.pomodoro import Pomodoro as PomodoroModel
 
@@ -10,6 +11,7 @@ from pomodorus.models.pomodoro import Pomodoro as PomodoroModel
 class Pomodoruses(Resource):
 
     @staticmethod
+    @jwt_required
     def get():
         return {
             'pomodoros': [p.json() for p in PomodoroModel.find_all()]
