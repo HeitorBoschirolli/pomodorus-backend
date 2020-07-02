9c23ffae4790272f37c168cf4113e1ac1e28eec8
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2020-07-02 12:15:32 -0300

    Require jwt to get intervals

--- pomodorus/resources/intervals.py
+++ pomodorus/resources/intervals.py
@@ -2,6 +2,7 @@
 Intervals resource. It contains information about many intervals
 """
 from flask_restful import Resource
+from flask_jwt_extended import jwt_required
 
 from pomodorus.models.interval import Interval as IntervalModel
 
@@ -9,6 +10,7 @@ from pomodorus.models.interval import Interval as IntervalModel
 class Intervals(Resource):
 
     @staticmethod
+    @jwt_required
     def get():
         return {
             'intervals': [i.json() for i in IntervalModel.find_all()]
