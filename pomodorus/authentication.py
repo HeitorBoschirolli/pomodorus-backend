a15598702386552e03208c2f329925ea138e2058
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2020-07-03 18:20:15 -0300

    Enable jwt blacklisting

--- pomodorus/authentication.py
+++ pomodorus/authentication.py
@@ -2,6 +2,7 @@
 Configure authentication some behaviors
 """
 from flask_jwt_extended import JWTManager
+from flask import jsonify
 
 from pomodorus.models.jwt_blacklist import JwtBlacklist
 
@@ -21,9 +22,20 @@ def setup_authentication(app, config):
     """
     app.config['JWT_SECRET_KEY'] = config['secret-key']
 
+    # enable jwt blacklisting
+    app.config['JWT_BLACKLIST_ENABLED'] = True
+    app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
     # enable jwt
     jwt = JWTManager(app)
 
     @jwt.token_in_blacklist_loader
     def is_token_in_blacklist(decrypted_token):
-        return JwtBlacklist.find_by_jid(decrypted_token['jid']) is None
+        return JwtBlacklist.find_by_jid(decrypted_token['jti']) is not None
+
+    @jwt.revoked_token_loader
+    def handle_revoke_token():
+        return jsonify({
+            'message': 'The provided token was revoked',
+            'error': 'TOKEN_REVOKED',
+        })
